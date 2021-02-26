ef03805b101ec70e8236ff6a51e60dad7abc80a9
calvinto14 <79028892+calvinto14@users.noreply.github.com>
2021-02-26 12:48:09 -0700

    Update GitDemo.java
    Swapped the factorial/reverse functions

--- GitDemo.java
+++ GitDemo.java
@@ -23,15 +23,11 @@ public class GitDemo {
      *  the function will return the value of (1 * 2 * 3 * 4)
      *****/
     public double findFactorial(int num) {
-        double factorialNumber = 0;
-        
-        while( num != 0 ) {
-            double temp = num % 10;
-            factorialNumber = factorialNumber * 10 + temp;
-            num /= 10;
-        }
-        
-        return factorialNumber;
+        double newNumber = 1;        
+        for(int i = 1; i <= num; ++i){
+            newNumber = newNumber * i;
+        }        
+        return newNumber;        
     }
     
     /*****
@@ -40,13 +36,12 @@ public class GitDemo {
      *  the function will return 4321
      *****/
     public double reverseNumber(int num){
-        double newNumber = 1;
-        
-        for(int i = 1; i <= num; ++i){
-            newNumber = newNumber * i;
+        double factorialNumber = 0;
+        while( num != 0 ) {
+            double temp = num % 10;
+            factorialNumber = factorialNumber * 10 + temp;
+            num /= 10;
         }
-        
-        return newNumber;
+        return factorialNumber;
     }
-    
 }
